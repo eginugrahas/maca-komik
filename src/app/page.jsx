ac77efd8e8bd69218d752fcbbf1bec2298c046ef
eginugrahas <eginugrahasmart@gmail.com>
2023-11-22 18:31:13 +0700

    setup

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { ThemeSwitcher } from "./Components/ThemeSwitcher";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Maca Komik</h1>
+    </>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,42 @@
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  darkMode: "class",
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            background: "#ecebf3", // or DEFAULT
+            foreground: "#11181C", // or 50 to 900 DEFAULT
+            primary: {
+              //... 50 to 900
+              foreground: "#ecebf3",
+              DEFAULT: "#006FEE",
+            },
+            // ... rest of the colors
+          },
+        },
+        dark: {
+          colors: {
+            background: "#0c120c", // or DEFAULT
+            foreground: "#ECEDEE", // or 50 to 900 DEFAULT
+            primary: {
+              //... 50 to 900
+              foreground: "#ecebf3",
+              DEFAULT: "#006FEE",
+            },
+          },
+          // ... rest of the colors
+        },
       },
-    },
-  },
-  plugins: [],
-}
+    }),
+  ],
+};
